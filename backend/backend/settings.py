9587fef90de677c71c281b0addb6c696f142804f
Md Sheik <mdsheik633@hotmail.com>
2024-03-20 10:22:22 -0400

    idk

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'your_database_name',
+        'USER': 'ghouse',
+        'PASSWORD': 'ghouse-scouting-Db22',
+        'HOST': '192.168.1.17',
+        'PORT': '5432',
     }
 }
 
@@ -116,7 +120,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
